0bfe2e0138063ae7c9c02a0c1e63c41bc575aa91
FlorFerr <florferreyra16@gmail.com>
2021-07-29 02:46:35 -0300

    imagen de fondo y logos de redes

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ $(document).ready(() =>{
         centerMode: true,
         centerPadding: '60px',
         slidesToShow: 3,
+        autoplay: true,
+        autoplaySpeed: 2000,
         
         responsive: [
           {
